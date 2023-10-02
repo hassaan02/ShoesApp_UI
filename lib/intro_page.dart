d7e9618408812dd3db4c006dc0eb7bde5832ce2a
hassaan02 <136227511+hassaan02@users.noreply.github.com>
2023-10-02 23:34:28 +0500

    initial commit

---
+++ lib/intro_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:nike_app/second_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset("lib/images/intro pic.png"),
+            ),
+            const Text(
+              "Your Journey Begins!",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 40),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xff0077b6)),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SecondPage(),
+                      ));
+                },
+                child: Text(
+                  "Get Started",
+                  style: TextStyle(fontSize: 20),
+                )),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:nike_app/intro_page.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
